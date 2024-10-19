42c5e1dd8fed6774b7cdbaa0ce3fad578c68d051
Tonko Mulder <tonko@tonkomulder.nl>
2024-10-19 03:12:06 +0200

    update after upstream background changes
    Signed-off-by: Tonko Mulder <tonko@tonkomulder.nl>

--- app/Views/pages/de/home.view.php
+++ app/Views/pages/de/home.view.php
@@ -3,6 +3,8 @@
     <main>
         <h1 class="text-2xl">Welcome to My Website</h1>
         <h2 class="text-xl">Titre plus petit</h2>
+
+        <x-background :url="$this->randomBackgroundUrl" />
     </main>
 
 </x-layout>
