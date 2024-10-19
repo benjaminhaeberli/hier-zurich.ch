d40c3a9f421e38cf9a1d8909cc0928140ea867aa
Tonko Mulder <tonko@tonkomulder.nl>
2024-10-19 02:46:51 +0200

    pass background image url via the helper
    Signed-off-by: Tonko Mulder <tonko@tonkomulder.nl>

--- app/PagesController.php
+++ app/PagesController.php
@@ -7,46 +7,76 @@ namespace App;
 use Tempest\Http\Get;
 use Tempest\View\View;
 
-use function Tempest\path;
 use function Tempest\view;
 
 final readonly class PagesController
 {
-    const PAGES_PATH = '/Views/pages/';
+    public const PAGES_PATH = '/Views/pages/';
 
     #[Get('/')]
-    public function hom(): View
+    public function home(): View
     {
-        return view(self::PAGES_PATH . 'home.view.php');
+        return view(self::PAGES_PATH . 'home.view.php')
+            ->data(
+                randomBackgroundUrl: $this->backgroundUrl(),
+            );
     }
 
     #[Get('/about')]
     public function about(): View
     {
-        return view(self::PAGES_PATH . 'about.view.php');
+        return view(self::PAGES_PATH . 'about.view.php')
+            ->data(
+                randomBackgroundUrl: $this->backgroundUrl(),
+            );
     }
 
     #[Get('/watch')]
     public function watch(): View
     {
-        return view(self::PAGES_PATH . 'watch.view.php');
+        return view(self::PAGES_PATH . 'watch.view.php')
+            ->data(
+                randomBackgroundUrl: $this->backgroundUrl(),
+            );
     }
 
     #[Get('/de')]
     public function homeDE(): View
     {
-        return view(self::PAGES_PATH . 'de/home.view.php');
+        return view(self::PAGES_PATH . 'de/home.view.php')
+            ->data(
+                randomBackgroundUrl: $this->backgroundUrl(),
+            );
     }
 
     #[Get('/de/about')]
     public function aboutDE(): View
     {
-        return view(self::PAGES_PATH . 'de/about.view.php');
+        return view(self::PAGES_PATH . 'de/about.view.php')
+            ->data(
+                randomBackgroundUrl: $this->backgroundUrl(),
+            );
     }
 
     #[Get('/de/watch')]
     public function watchDE(): View
     {
-        return view(self::PAGES_PATH . 'de/watch.view.php');
+        return view(self::PAGES_PATH . 'de/watch.view.php')
+            ->data(
+                randomBackgroundUrl: $this->backgroundUrl(),
+            );
+    }
+
+    private function backgroundUrl(): string
+    {
+        /** @var string $url */
+        $url = collect([
+            '/images/fossile-1.jpg',
+            '/images/fossile-2.jpg',
+            '/images/river-1.jpg',
+        ])
+            ->random();
+
+        return $url;
     }
 }
